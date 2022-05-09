c1f7119ea2f62ad890f9274044cb8ea21a39a499
unknown <you@example.com>
2022-05-09 20:01:21 +0600

    inventory add

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ require('dotenv').config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j1owr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 async function run() {
 
-    try {
+try {
 
         await client.connect();
         console.log('db connected');
